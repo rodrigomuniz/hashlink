0d6e8f8986852f943f7f711c9f94ffbe7606a9f1
rodriggomuniz <rodriggomuniz@2f43d2f2-2149-9a3c-3149-d0d068a2f6a2>
2010-05-29 03:06:16 +0000

    Adicionando os arquivos depois do DOJO

---
+++ js/hash-link.js
@@ -0,0 +1,14 @@
+/* jQuery.fn.hashLink = function(o) {
+ 
+ // Defaults
+ var o = jQuery.extend( {
+ var1: 'some_variable',
+ var2: 'some_variable',
+ var3: 'some_variable'
+ },o);
+ 
+ $(this).each(function(){
+   // Perform actions on item here
+ });
+ 
+}; */
